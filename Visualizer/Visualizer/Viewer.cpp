a3747da1b086f58cefdc73b97463624584a21682
framled <fn.narvaezmichea@gmail.com>
2015-10-31 03:40:01 -0300

    First Commit added Properties page:     PCL-1.7.2

---
+++ Visualizer/Visualizer/Viewer.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Viewer.h"
+
+
+Viewer::Viewer()
+{
+}
+
+
+Viewer::~Viewer()
+{
+}
---
+++ Visualizer/Visualizer/Viewer.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+class Viewer
+{
+public:
+	Viewer();
+	~Viewer();
+};
+
